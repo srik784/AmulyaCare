98df2b95335ff835489daa0cc85d39e1e8b230d4
admin <cdastagirireddy@hotmail.com>
2018-02-07 12:49:58 +0530

    casualy

--- app/src/main/java/com/k/amulyacare/MainActivity.java
+++ app/src/main/java/com/k/amulyacare/MainActivity.java
@@ -16,6 +16,9 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
