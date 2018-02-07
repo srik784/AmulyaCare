eb548a1a40e8277e096c9f24533e52c160cff87c
admin <cdastagirireddy@hotmail.com>
2018-02-07 15:14:32 +0530

    casualy

--- app/src/main/java/com/k/amulyacare/MainActivity.java
+++ app/src/main/java/com/k/amulyacare/MainActivity.java
@@ -80,17 +80,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_aboutus) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_services) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_treatments) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_contact) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_consult) {
 
         }
 
