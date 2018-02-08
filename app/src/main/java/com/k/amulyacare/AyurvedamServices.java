8a926a73ab37e6d621d33459f49fb22ce2775b1a
admin <cdastagirireddy@hotmail.com>
2018-02-08 18:06:13 +0530

    Services classes Ayurvedam, Beautycare and Ladiesfitness classes

---
+++ app/src/main/java/com/k/amulyacare/AyurvedamServices.java
@@ -0,0 +1,24 @@
+package com.k.amulyacare;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by admin on 2/8/2018.
+ */
+
+public class AyurvedamServices extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View v=inflater.inflate(R.layout.ayurvedam_services,null);
+
+        return v;
+    }
+}
