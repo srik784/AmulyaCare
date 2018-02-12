e11b325a4e19296b383bb249ab88c254659db832
swarnalatharouthu <swarnalatharouthu@gmail.com>
2018-02-12 12:18:50 +0530

    treatments

--- app/src/main/java/com/k/amulyacare/Treatments.java
+++ app/src/main/java/com/k/amulyacare/Treatments.java
@@ -1,25 +1,16 @@
 package com.k.amulyacare;
 
-import android.os.Build;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Layout;
 import android.widget.TextView;
 
-import static android.text.Layout.JUSTIFICATION_MODE_INTER_WORD;
-
 public class Treatments extends AppCompatActivity {
-    TextView tv;
 
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_treatments);
-        tv=(TextView)findViewById(R.id.sirodhara);
-        tv.setJustificationMode(JUSTIFICATION_MODE_INTER_WORD);
 
 
 
