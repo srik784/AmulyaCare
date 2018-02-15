ce5ab014e3a4ce0024f7bcd00a80e1ab1337b927
priyam1996 <priyankareddythummala@gmail.com>
2018-02-15 14:40:40 +0530

    specialities

--- app/src/main/java/com/k/amulyacare/Specialities.java
+++ app/src/main/java/com/k/amulyacare/Specialities.java
@@ -1,23 +1,18 @@
 package com.k.amulyacare;
 
-import android.support.design.widget.TabLayout;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-
-import android.widget.TextView;
 
 public class Specialities extends AppCompatActivity {
 
@@ -88,40 +83,6 @@ public class Specialities extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_specialities2, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
@@ -137,24 +98,57 @@ public class Specialities extends AppCompatActivity {
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            switch (position){
+                case 0:
+                    Obesity obesity=new Obesity();
+                    return obesity;
+                case 1:
+                    Stress stress=new Stress();
+                    return  stress;
+                case 2:
+                    LowBackPain lowBackPain=new LowBackPain();
+                    return  lowBackPain;
+                case 3:
+                    CervicalPain cervicalpain=new CervicalPain();
+                    return  cervicalpain;
+                case 4:
+                    Arthritis arthritis=new Arthritis();
+                    return  arthritis;
+                case 5:
+                    Paralysis paralysis=new Paralysis();
+                    return  paralysis;
+                case 6:
+                    Piles piles=new Piles();
+                    return  piles;
+                default:
+                    return null;
+            }
+
         }
 
         @Override
         public int getCount() {
             // Show 3 total pages.
-            return 3;
+            return 7;
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
             switch (position) {
                 case 0:
-                    return "SECTION 1";
+                    return "Obesity";
                 case 1:
-                    return "SECTION 2";
+                    return "Stress";
                 case 2:
-                    return "SECTION 3";
+                    return "Low Back Pain";
+                case 3:
+                    return "Cervical Pain";
+                case 4:
+                    return "Arthritis";
+                case 5:
+                    return "Paralysis";
+                case 6:
+                    return "Piles";
             }
             return null;
         }
