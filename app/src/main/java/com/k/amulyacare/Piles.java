776492b9def6cb8c752e67260da7d677da2767ec
priyam1996 <priyankareddythummala@gmail.com>
2018-02-15 12:44:18 +0530

    piles

---
+++ app/src/main/java/com/k/amulyacare/Piles.java
@@ -0,0 +1,25 @@
+package com.k.amulyacare;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by abc on 2/15/2018.
+ */
+
+@SuppressLint("ValidFragment")
+class Piles extends Fragment{
+
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.piles, null);
+
+        return v;
+    }
+
+}
