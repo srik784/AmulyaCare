48bf84d16554f61742ad72e2b2efde512fa90528
priyam1996 <priyankareddythummala@gmail.com>
2018-02-15 12:43:56 +0530

    Stress

---
+++ app/src/main/java/com/k/amulyacare/Stress.java
@@ -0,0 +1,23 @@
+package com.k.amulyacare;
+
+import android.annotation.SuppressLint;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by abc on 2/15/2018.
+ */
+
+@SuppressLint("ValidFragment")
+class Stress extends Fragment{
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.stress, null);
+
+        return v;
+    }
+}
