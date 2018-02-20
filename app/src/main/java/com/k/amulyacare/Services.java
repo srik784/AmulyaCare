30cb2f359420c5723ecc2aed13e61f20fec41c6e
priyam1996 <priyankareddythummala@gmail.com>
2018-02-20 13:17:41 +0530

    services

--- app/src/main/java/com/k/amulyacare/Services.java
+++ app/src/main/java/com/k/amulyacare/Services.java
@@ -1,24 +1,16 @@
 package com.k.amulyacare;
 
-import android.support.annotation.Nullable;
-import android.support.design.widget.TabLayout;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-
-import android.widget.TextView;
 
 public class Services extends AppCompatActivity {
 
@@ -36,7 +28,9 @@ public class Services extends AppCompatActivity {
      * The {@link ViewPager} that will host the section contents.
      */
     private ViewPager mViewPager;
-
+    AyurvedamServices ayurvedam;
+    BeautycareServices beautyCare;
+    LadiesfitnessServices ladiesFitness;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,6 +54,10 @@ public class Services extends AppCompatActivity {
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 
 
+
+        ayurvedam=new AyurvedamServices();
+        ladiesFitness=new LadiesfitnessServices();
+        beautyCare=new BeautycareServices();
     }
 
 
@@ -107,13 +105,13 @@ public class Services extends AppCompatActivity {
             // Return a PlaceholderFragment (defined as a static inner class below).
             switch (position){
                 case 0:
-                    AyurvedamServices ayurvedam=new AyurvedamServices();
+
                     return ayurvedam;
                 case 1:
-                    LadiesfitnessServices ladiesFitness=new LadiesfitnessServices();
+
                     return ladiesFitness;
                 case 2:
-                    BeautycareServices beautyCare=new BeautycareServices();
+
                     return beautyCare;
                 default:
                     return null;
